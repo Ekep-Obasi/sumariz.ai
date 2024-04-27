d30adf90f4be92a373f5806764826934ed4fb18f
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2024-04-27 21:11:31 +0100

    feat: wrote helper to extract text from images

---
+++ src/utils/image-to-text.ts
@@ -0,0 +1,8 @@
+import { createWorker } from "tesseract.js";
+
+export const convertImageToText = async (image: string): Promise<string> => {
+  const worker = await createWorker("eng");
+  const ret = await worker.recognize(image);
+  await worker.terminate();
+  return ret.data.text;
+};
