aed1e1ba23216204d36accc9e14ee0025cd35821
ARREY-ETTA BESSONG EKEP OBASI <arreyettaekep@gmail.com>
2024-08-05 01:18:06 +0100

    perf: switched to openai

--- src/utils/summarize.ts
+++ src/utils/summarize.ts
@@ -1,9 +1,20 @@
-import { pipeline } from '@xenova/transformers'
+import { OpenAI } from '@langchain/openai'
+import { loadSummarizationChain } from 'langchain/chains'
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 
 export const summarizeText = async (text: string) => {
-  const pipe = await pipeline('summarization')
+  const model = new OpenAI({
+    temperature: 0,
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  })
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000, chunkOverlap: 500 })
+  const docs = await textSplitter.createDocuments([text])
 
-  const summarizationOutput = await pipe(text, { max_length: text.length / 2 })
+  const chain = loadSummarizationChain(model, { type: 'map_reduce' })
 
-  return summarizationOutput
+  const result = await chain.invoke({
+    input_documents: docs,
+  })
+
+  return result
 }
